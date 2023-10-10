4b826b4f8e539e0f06d4578d630e92206cc5bced
Joao-PauloPa412 <26451881-Joao-PauloPa412@users.noreply.replit.com>
2023-10-10 00:33:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Atividade da aula de automação de 09/10/2023
+
+const Aluno01 = 'Jonas'
+const Aluno02 = 'Maria'
+
+const nota_aluno01 = 9;
+const nota_aluno02 = 8;
+
+console.log('Olá ' + Aluno01 + ' Sua nota é ' + nota_aluno01 )
+
+console.log('Olá ' + Aluno02 + ' Sua nota é ' + nota_aluno02 )
